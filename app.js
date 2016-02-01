e774acd87c742c5cde8cd5ba4c70479280a4a044
Damian Lajara <damiansito93@gmail.com>
2016-02-01 10:35:34 -0500

    add more attr to user model and refactor error handling for db connection

--- app.js
+++ app.js
@@ -8,27 +8,28 @@ var morgan = require('morgan'); // Log all requests
 var dbConfig = require('./app/db/config/db.js');
 var mongoose = require('mongoose');
 var methodOverride = require('method-override');
-var app = express();
+//var favicon = require('serve-favicon'); // Make sure to install first before uncommenting this
 
 // Configure the app
+// app.use(favicon(__dirname + '/public/favicon.ico')); // Make sure to put the favicon.ico in the public dir
 app.use(bodyParser.urlencoded({extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev')); //Or app.use(morgan('combined'));
 app.use(methodOverride('X-HTTP-Method-Override')); // Simulate PUT and DELETE http action verbs
 app.use(express.static(path.join(__dirname, '/public'))); // Set the static root mounting point (/javascripts instead of public/javascripts)
 app.use('/vendor-css', express.static(path.join(__dirname, 'node_modules/')));
-// For the view engine
-// /app.set('views', path.join(__dirname, 'views'));
+
+// For the view engine (Make sure to install first)
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'mustache');
+// app.engine('mustache', require('hogan_middleware').__express);
 
 // Connect to the database
-mongoose.connect(dbConfig.connection);
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Mongo connection error:'));
-db.once('open', function() {
-    console.log('we\'re connected!');
+mongoose.connect(dbConfig.connection, function(err,res) {
+    err ? console.log('Mongo connection error on: ' +  dbConfig.connection, err) : console.log('Succeeded connecting to: ' + dbConfig.connection)
 });
 
+var app = express();
 
 // Register our routes to their respective handlers
 app.use('/', defaultRoute);
--- app/models/user.js
+++ app/models/user.js
@@ -2,8 +2,16 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-    name: String,
-    default: ''
+    firstName: {type: String, trim:true, default: ''},
+    lastName: {type: String, trim:true, default: ''},
+    username: {type: String, trim:true, default: ''},
+    email: {type: String, trim:true,  default: ''},
+    timestamp: {type: Date, default: Date.now }
+
 });
+
+userSchema.methods.name = function() {
+    return this.firstName + ' ' + this.lastName;
+};
 
 module.exports = mongoose.model('User', userSchema);
