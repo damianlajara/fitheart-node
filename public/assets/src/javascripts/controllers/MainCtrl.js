4e647cefc3330fd2b280b7936691bf765f6297ad
Damian Lajara <damiansito93@gmail.com>
2016-02-04 08:36:54 -0500

    add more sidebar options and icons

--- public/assets/src/javascripts/controllers/MainCtrl.js
+++ public/assets/src/javascripts/controllers/MainCtrl.js
@@ -4,34 +4,68 @@ angular.module('MainCtrl',[])
             console.log("clicked on toggle button");
             $mdSidenav('left').toggle();
         };
-        $scope.menu = [
-            {
-                link : '',
-                title: 'Dashboard',
-                icon: 'dashboard'
-            },
-            {
-                link : '',
-                title: 'Friends',
-                icon: 'group'
-            },
-            {
-                link : '',
-                title: 'Messages',
-                icon: 'message'
-            }
-        ];
-        $scope.admin = [
-            {
-                link : '',
-                title: 'Trash',
-                icon: 'delete'
-            },
-            {
-                link : 'showListBottomSheet($event)',
-                title: 'Settings',
-                icon: 'settings'
-            }
-        ];
+        // ICONS: https://design.google.com/icons/
+        var menu = {
+            socialMenu: [
+                {
+                    link : '',
+                    title: 'Dashboard',
+                    icon: 'dashboard'
+                },
+                {
+                    link : '',
+                    title: 'Friends',
+                    icon: 'group'
+                },
+                {
+                    link : '',
+                    title: 'Messages',
+                    icon: 'message'
+                }
+            ],
+            healthMenu: [
+                {
+                    link : '',
+                    title: 'Fitness',
+                    icon: 'rowing'
+                },
+                {
+                    link : '',
+                    title: 'Nutrition',
+                    icon: 'battery_charging_full'
+                },
+                {
+                    link : '',
+                    title: 'Goals',
+                    icon: 'timeline'
+                },
+                {
+                    link : '',
+                    title: 'Meal Plan',
+                    icon: 'assignment'
+                },
+                {
+                    link : '',
+                    title: 'Mood',
+                    icon: 'insert_emoticon'
+                }
+            ],
+            adminMenu: [
+                {
+                    link : '',
+                    title: 'Trash',
+                    icon: 'delete'
+                },
+                {
+                    link : '',
+                    title: 'Settings',
+                    icon: 'settings'
+                }
+            ]
+        };
+
+        $scope.socialMenu = menu.socialMenu;
+        $scope.healthMenu = menu.healthMenu;
+        $scope.adminMenu = menu.adminMenu;
     }]);
 
