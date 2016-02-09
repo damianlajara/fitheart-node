63e6a59ac4b011c5f8f3988ed4f152499ff05329
Damian Lajara <damiansito93@gmail.com>
2016-02-09 14:40:22 -0500

    add chartjs to angular app module as dependent

--- public/assets/src/javascripts/angularApp.js
+++ public/assets/src/javascripts/angularApp.js
@@ -2,10 +2,11 @@ var angular = require('angular');
 require('angular-route');
 require('jquery'); // Material needs jquery
 require('angular-material');
+require('angular-chartjs');
 
 // Require directory only so browserify can search for the index.js file by default and require it's dependencies
 require('./controllers/index');
 require('./routes/index');
 require('./services/index');
 
-angular.module('fitHeart', ['ngMaterial', 'ngRoute', 'frontEndRoutes', 'MainCtrl', 'UserCtrl', 'UserService']);
+angular.module('fitHeart', ['chartjs', 'ngMaterial', 'ngRoute', 'frontEndRoutes', 'MainCtrl', 'UserCtrl', 'UserService']);
